ca75c58695b81215c36a344c87884a36f8626e16
Racknar0 <racknarow1@gmail.com>
2022-03-01 12:50:56 -0500

    Fill Object

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+};
 
 // Crear un Promise
 const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
@@ -9,6 +16,11 @@ const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
 
 document.addEventListener('DOMContentLoaded', () => {
     consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario);
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
 })
 
 function consultarCriptomonedas() {
@@ -30,4 +42,28 @@ function consultarCriptomonedas() {
 
         criptomonedasSelect.appendChild(option);
     })
+ }
+
+
+ function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+ }
+
+
+ function submitFormulario ( e ) {
+    e.preventDefault();
+    
+    //! validar form
+    const {moneda, criptomoneda}  = objBusqueda;
+
+    if( moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+ }
+
+
+ 
+ function mostrarAlerta (mensaje) {
+    console.log(mensaje);
  }
